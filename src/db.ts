a06c745f5a0e0e84525b62badc33458b6bd33e89
Brayan Paucar <joan300501@gmail.com>
2026-08-18 09:58:57 -0500

    Improvements and bug fixes

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,5 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
 
-config({ path: ".env" }); // or .env.local
-
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(import.meta.env.VITE_DATABASE_URL as string);
 export const db = drizzle({ client: sql });
